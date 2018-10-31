e0f87e624588a7506d1fedd16d9400670237dfcc
Calos Lopez <carlopex97@gmail.com>
2018-10-31 13:16:51 +0100

    Update ionic_gta/src/app/app.firebase.config.ts

---
+++ ionic_gta/src/app/app.firebase.config.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = var config = {
+    apiKey: "AIzaSyC0aOCSoeLt1Pja1F-Rk1IrUFzh7cKmrNY",
+    authDomain: "ionicgta.firebaseapp.com",
+    databaseURL: "https://ionicgta.firebaseio.com",
+    projectId: "ionicgta",
+    storageBucket: "ionicgta.appspot.com",
+    messagingSenderId: "570167287361"
+  };
